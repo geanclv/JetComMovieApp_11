79a0d5b392ae501ab45d13dfefcaa1980bb0611a
geanclv <geanclv@gmail.com>
2022-07-20 14:05:50 -0500

    Defining the main screen layout.

--- app/src/main/java/com/geancarloleiva/jetcommovieapp_11/MainActivity.kt
+++ app/src/main/java/com/geancarloleiva/jetcommovieapp_11/MainActivity.kt
@@ -3,11 +3,20 @@ package com.geancarloleiva.jetcommovieapp_11
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountBox
+import androidx.compose.material.icons.filled.Home
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.geancarloleiva.jetcommovieapp_11.ui.theme.JetComMovieApp_11Theme
@@ -16,15 +25,13 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MovieApp {
-                MainContent()
-            }
+            MovieApp()
         }
     }
 }
 
 @Composable
-fun MovieApp(content: @Composable () -> Unit) {
+fun MovieApp() {
     JetComMovieApp_11Theme {
         Scaffold(topBar = {
             TopAppBar(
@@ -34,12 +41,57 @@ fun MovieApp(content: @Composable () -> Unit) {
                 Text(text = "Movie App")
             }
         }) {
-            content
+            MainContent()
         }
     }
 }
 
 @Composable
-fun MainContent() {
+fun MainContent(lstMovie: List<String> = listOf("300", "200")) {
+    Column(modifier = Modifier.padding(12.dp)) {
+        LazyColumn {
+            items(items = lstMovie) {
+                MovieRow(movie = it){ movie ->
 
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun MovieRow(movie: String, onItemClick: (String) -> Unit) {
+    Card(
+        modifier = Modifier
+            .padding(4.dp)
+            .fillMaxWidth()
+            .height(130.dp)
+            .clickable {
+                onItemClick(movie)
+            },
+        shape = RoundedCornerShape(corner = CornerSize(16.dp)),
+        elevation = 6.dp
+    ) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Start
+        ) {
+            Surface(
+                modifier = Modifier
+                    .padding(12.dp)
+                    .size(100.dp),
+                shape = RectangleShape,
+                elevation = 4.dp
+            ) {
+                Icon(imageVector = Icons.Default.Home, contentDescription = movie)
+            }
+            Text(text = movie)
+        }
+    }
+}
+
+@Preview(showBackground = false)
+@Composable
+fun preview() {
+    MovieApp ()
 }
