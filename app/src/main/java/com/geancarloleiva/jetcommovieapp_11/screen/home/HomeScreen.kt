0e351f98249e96dcf2881e3e4deb363eaa8ac9d5
geanclv <geanclv@gmail.com>
2022-07-25 22:37:05 -0500

    Adding "More Detail" arrow in order to show or hide more details about a row.

--- app/src/main/java/com/geancarloleiva/jetcommovieapp_11/screen/home/HomeScreen.kt
+++ app/src/main/java/com/geancarloleiva/jetcommovieapp_11/screen/home/HomeScreen.kt
@@ -1,5 +1,6 @@
 package com.geancarloleiva.jetcommovieapp_11.screen.home
 
+import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
@@ -8,13 +9,21 @@ import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
-import androidx.compose.runtime.Composable
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.KeyboardArrowDown
+import androidx.compose.material.icons.filled.KeyboardArrowUp
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import coil.compose.rememberAsyncImagePainter
 import coil.request.ImageRequest
@@ -64,7 +73,7 @@ fun MovieRow(
         modifier = Modifier
             .padding(4.dp)
             .fillMaxWidth()
-            .height(130.dp)
+            //.height(130.dp) //the height should be dynamic when I expand or contract the "more details"
             .clickable {
                 onItemClick(movie.id)
             },
@@ -82,6 +91,10 @@ fun MovieRow(
 
 @Composable
 fun MovieDetail(movie: Movie) {
+    var stateExpanded by remember {
+        mutableStateOf(false)
+    }
+
     Surface(
         modifier = Modifier
             .padding(12.dp)
@@ -115,6 +128,56 @@ fun MovieDetail(movie: Movie) {
         Text(
             text = "Release: ${movie.year}",
             style = MaterialTheme.typography.caption
+        )
+        //Additional info, by default hidden
+        AnimatedVisibility(visible = stateExpanded) {
+            Column {
+                Text(
+                    buildAnnotatedString {
+                        withStyle(
+                            style = SpanStyle(
+                                color = Color.DarkGray,
+                                fontSize = 13.sp
+                            )
+                        ) {
+                            append("Plot: ")
+                        }
+                        withStyle(
+                            style = SpanStyle(
+                                color = Color.DarkGray, fontSize = 13.sp,
+                                fontWeight = FontWeight.Light
+                            )
+                        ) {
+                            append(movie.plot)
+                        }
+                    }, modifier = Modifier.padding(6.dp)
+                )
+                Divider()
+                Text(
+                    text = "Director: ${movie.director}",
+                    style = MaterialTheme.typography.caption
+                )
+                Text(
+                    text = "Actors: ${movie.actors}",
+                    style = MaterialTheme.typography.caption
+                )
+                Text(
+                    text = "Rating: ${movie.rating}",
+                    style = MaterialTheme.typography.caption
+                )
+            }
+        }
+
+        //Icon to see more info about the movie
+        Icon(
+            imageVector = if (stateExpanded) Icons.Filled.KeyboardArrowUp else Icons.Filled.KeyboardArrowDown,
+            contentDescription = "More info",
+            modifier = Modifier
+                .size(25.dp)
+                .clickable {
+                    stateExpanded = !stateExpanded
+                },
+            tint = Color.DarkGray
         )
     }
 }
