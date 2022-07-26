76148084a36a8de974ea4f08aa5c58b1ee70f5c4
geanclv <geanclv@gmail.com>
2022-07-25 22:18:47 -0500

    Creating a Data Class that contain the Movie detail. Modifying the MovieList in order to obtain the information from the Movie Class. Loading images with Coil.

--- app/src/main/java/com/geancarloleiva/jetcommovieapp_11/screen/home/HomeScreen.kt
+++ app/src/main/java/com/geancarloleiva/jetcommovieapp_11/screen/home/HomeScreen.kt
@@ -1,5 +1,6 @@
 package com.geancarloleiva.jetcommovieapp_11.screen.home
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
@@ -7,18 +8,23 @@ import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Home
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import coil.compose.rememberAsyncImagePainter
+import coil.request.ImageRequest
+import coil.transform.CircleCropTransformation
+import com.geancarloleiva.jetcommovieapp_11.model.Movie
+import com.geancarloleiva.jetcommovieapp_11.model.getMovies
 import com.geancarloleiva.jetcommovieapp_11.navigation.MovieScreens
 
 @Composable
-fun HomeScreen(navController: NavController){
+fun HomeScreen(navController: NavController) {
     Scaffold(topBar = {
         TopAppBar(
             backgroundColor = MaterialTheme.colors.primary,
@@ -32,14 +38,17 @@ fun HomeScreen(navController: NavController){
 }
 
 @Composable
-fun MainContent(navController: NavController,
-    lstMovie: List<String> = listOf("300", "200", "200", "200", "200", "200", "200", "200")) {
+fun MainContent(
+    navController: NavController,
+    lstMovie: List<Movie> = getMovies()
+) {
     Column(modifier = Modifier.padding(12.dp)) {
         LazyColumn {
             items(items = lstMovie) {
-                MovieRow(movie = it){ movie ->
+                MovieRow(movie = it) { movie ->
                     navController.navigate(
-                        route = MovieScreens.DetailScreen.name+"/$movie")
+                        route = MovieScreens.DetailScreen.name + "/$movie"
+                    )
                 }
             }
         }
@@ -47,14 +56,17 @@ fun MainContent(navController: NavController,
 }
 
 @Composable
-fun MovieRow(movie: String, onItemClick: (String) -> Unit) {
+fun MovieRow(
+    movie: Movie,
+    onItemClick: (String) -> Unit
+) {
     Card(
         modifier = Modifier
             .padding(4.dp)
             .fillMaxWidth()
             .height(130.dp)
             .clickable {
-                onItemClick(movie)
+                onItemClick(movie.id)
             },
         shape = RoundedCornerShape(corner = CornerSize(16.dp)),
         elevation = 6.dp
@@ -63,16 +75,46 @@ fun MovieRow(movie: String, onItemClick: (String) -> Unit) {
             verticalAlignment = Alignment.CenterVertically,
             horizontalArrangement = Arrangement.Start
         ) {
-            Surface(
-                modifier = Modifier
-                    .padding(12.dp)
-                    .size(100.dp),
-                shape = RectangleShape,
-                elevation = 4.dp
-            ) {
-                Icon(imageVector = Icons.Default.Home, contentDescription = movie)
-            }
-            Text(text = movie)
+            MovieDetail(movie = movie)
         }
+    }
+}
+
+@Composable
+fun MovieDetail(movie: Movie) {
+    Surface(
+        modifier = Modifier
+            .padding(12.dp)
+            .size(100.dp),
+        shape = RectangleShape,
+        elevation = 4.dp
+    ) {
+        Image(
+            //Only loading the image
+            //painter = rememberAsyncImagePainter(model = movie.images[0]),
+            //Loading with a crossfade during loading
+            painter = rememberAsyncImagePainter(
+                model = ImageRequest.Builder(LocalContext.current)
+                    .data(movie.images[0])
+                    .crossfade(true)
+                    .transformations(CircleCropTransformation())
+                    .build()
+            ),
+            contentDescription = movie.title
+        )
+    }
+    Column(modifier = Modifier.padding(4.dp)) {
+        Text(
+            text = movie.title,
+            style = MaterialTheme.typography.h6
+        )
+        Text(
+            text = "Director: ${movie.director}",
+            style = MaterialTheme.typography.caption
+        )
+        Text(
+            text = "Release: ${movie.year}",
+            style = MaterialTheme.typography.caption
+        )
     }
 }
